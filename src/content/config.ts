aded99d87f2c219b2d40774f7590300a14875dce
Quintisimo <quintuscardozo13@gmail.com>
2023-09-17 05:10:34 +1000

    create astro site

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: ({ image }) => z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		heroImage: image(),
+	}),
+});
+
+export const collections = { blog };
