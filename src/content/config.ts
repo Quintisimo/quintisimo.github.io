09d371bde817fde70068c4f717b17b09dabdb679
Quintisimo <quintuscardozo13@gmail.com>
2023-09-21 01:35:20 +1000

    add first blog post

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,24 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
+
+const firstLetterCapital = (str: string) =>
+  str.charAt(0) === str.charAt(0).toUpperCase();
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		heroImage: image(),
-	}),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z
+        .string()
+        .refine((val) => val.split(" ").every(firstLetterCapital), {
+          message: "Use parcal case",
+        }),
+      description: z.string().refine(firstLetterCapital, {
+        message: "First letter should be capital",
+      }),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      heroImage: image(),
+    }),
 });
 
 export const collections = { blog };
