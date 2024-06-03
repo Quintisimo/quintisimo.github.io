2814e9133a91f20f6ac0b7ccb8b51261e3eaf25e
Quintus Cardozo <qcardozo@getlegaltech.com>
2024-06-04 00:45:37 +1000

    Convert rss file to ts

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,20 @@
+import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+
+export async function GET(context: APIContext) {
+  const posts = await getCollection("blog");
+  if (context.site) {
+    return rss({
+      title: SITE_TITLE,
+      description: SITE_DESCRIPTION,
+      site: context.site,
+      items: posts.map((post) => ({
+        ...post.data,
+        link: `/blog/${post.slug}/`,
+      })),
+    });
+  }
+  return new Response(null);
+}
