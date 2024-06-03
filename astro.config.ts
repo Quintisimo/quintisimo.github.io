5fd718872d57a3e3cb1e81c2186895f0674c9dd9
Quintus Cardozo <qcardozo@getlegaltech.com>
2024-06-04 00:34:03 +1000

    Add site url

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://quintuscardozo.dev",
   markdown: {
     shikiConfig: {
       theme: "vitesse-dark",
